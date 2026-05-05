a304ac596feb1fd02b5362fa1609f51789dbc6a0
Gabriel <00001106883950sp@gmail.com>
2026-05-05 16:38:38 -0300

    add: adiciona campo de idade e filhos no banco de dados

---
+++ mobile/test_app/aplicativo-test/service/api.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://10.90.189.230:3000/api",
+    headers: {"Content-Type": "application/json"},
+    // Opcional
+    timeout: 5000,
+});
+
+export default api;
